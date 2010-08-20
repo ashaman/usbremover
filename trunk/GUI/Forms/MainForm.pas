d56fc181c8217a0088cf6cd9ac4845033c50063a
DarthYarius_0990@mail.ru <DarthYarius_0990@mail.ru@8417aba2-bd37-11de-8784-f7ad4773dbab>
2010-08-20 19:59:45 +0000

    Updates: 1) Started UI creation 2) Added popup menus Planned: 1) Controller for MainWnd 2) Settings manager and Settings window 3) About and Help windows

--- trunk/GUI/Forms/MainForm.pas
+++ trunk/GUI/Forms/MainForm.pas
@@ -19,12 +19,21 @@ uses
 
 type
 
+
+    //TODO: implement form controller
+
   { TMainWnd }
 
   TMainWnd = class(TForm)
     ImageList: TImageList;
     MainMenu: TMainMenu;
     MenuItem1: TMenuItem;
+    MenuItem2: TMenuItem;
+    TApmExit: TMenuItem;
+    TApmSeparator: TMenuItem;
+    TApmShowAboutDlg: TMenuItem;
+    TApmShowSettings: TMenuItem;
+    TApmShowMainWnd: TMenuItem;
     MIViewHideToolbar: TMenuItem;
     MIViewRefresh: TMenuItem;
     MIHelpAbout: TMenuItem;
@@ -33,6 +42,7 @@ type
     MIView: TMenuItem;
     MIHelp: TMenuItem;
     MICommandExit: TMenuItem;
+    TrayAppPopupMenu: TPopupMenu;
     TrayDisksPopupMenu: TPopupMenu;
     ToolBar: TToolBar;
     TrayIcon: TTrayIcon;
@@ -47,7 +57,11 @@ type
     procedure MIHelpIndexClick(Sender: TObject);
     procedure MIViewHideToolbarClick(Sender: TObject);
     procedure MIViewRefreshClick(Sender: TObject);
+    procedure TApmExitClick(Sender: TObject);
+    procedure TApmShowMainWndClick(Sender: TObject);
     procedure TrayIconDblClick(Sender: TObject);
+    procedure TrayIconMouseDown(Sender: TObject; Button: TMouseButton;
+      Shift: TShiftState; X, Y: Integer);
   private
     { private declarations }
   public
@@ -67,39 +81,62 @@ var
 //Shows the main form after a double click on the application
 procedure TMainWnd.TrayIconDblClick(Sender: TObject);
 begin
-  Show;
+    Self.Show;
 end; //TMainWnd.TrayIconDblClick
+
+//Connects the appropriate popup menu to the tray icon
+procedure TMainWnd.TrayIconMouseDown(Sender: TObject; Button: TMouseButton;
+  Shift: TShiftState; X, Y: Integer);
+begin
+    //for left-button click
+    if (Button = mbLeft)
+    then begin
+        //connecting disk ejection menu
+        Self.TrayIcon.PopUpMenu := Self.TrayDisksPopupMenu;
+        //TODO: add info to the menu and add click handlers
+    end
+    //for right-button click
+    else if (Button = mbRight)
+    then begin
+        //attaching application popup menu
+        Self.TrayIcon.PopUpMenu := Self.TrayAppPopupMenu;
+        //TODO: add info to the menu
+        //TODO: add images to image list and use them
+    end;
+end; //TMainWnd.TrayIconMouseDown
 
 //Closing handling. If the option "Exit On Close" is set,
 //then the aplication's window will be closed. Otherwise,
 //the form will be hidden to the tray
 procedure TMainWnd.FormCloseQuery(Sender: TObject; var CanClose: boolean);
 begin
-  //close app GUI
-  if (ExitOnClose = true)
-  then begin
-    CanClose := true;
-  end //then - ExitOnClose is true
-  //hide to tray
-  else begin
-    CanClose := false;
-    Self.Hide;
-  end; //else - ExitOnClose is false
+    //close app GUI
+    if (ExitOnClose = true)
+    then begin
+        CanClose := true;
+    end //then - ExitOnClose is true
+    //hide to tray
+    else begin
+        CanClose := false;
+        Self.Hide;
+    end; //else - ExitOnClose is false
 end; //FormCloseQuery
 
+//Form creation event handler
 procedure TMainWnd.FormCreate(Sender: TObject);
 begin
     //creating the connector
     pconnector := TPipeConnector.Create;
     //refreshing the device list
     pconnector.Refresh;
-end;
+end; //TMainWnd.FormCreate
 
+//Form destruction event handler
 procedure TMainWnd.FormDestroy(Sender: TObject);
 begin
     //calling the connector's destructor
     pconnector.Destroy;
-end;
+end; //TMainWnd.FormDestroy
 
 //Form hiding handler. Hides the main form and shows the program
 //icon in the tray area
@@ -123,20 +160,23 @@ begin
   self.Close;
 end; //TMainWnd.MICommandExitClick
 
+//Shows the information about the application
 procedure TMainWnd.MIHelpAboutClick(Sender: TObject);
 begin
     //TODO: put a box with about information
-end;
+end; //TMainWnd.MIHelpAboutClick
 
+//Shows the help
 procedure TMainWnd.MIHelpIndexClick(Sender: TObject);
 begin
     Application.MessageBox('Sorry, not implemented yet :(',
         'USBRemover', MB_OK or MB_ICONEXCLAMATION);
      //TODO: put some code here
-end;
+end; //TMainWnd.MIHelpIndexClick
 
 procedure TMainWnd.MIViewHideToolbarClick(Sender: TObject);
 begin
+    //TODO: do it better - with string constants (lists, etc.)
      if (self.ToolBar.Visible)
      then begin
           self.ToolBar.Hide;
@@ -148,10 +188,23 @@ begin
      end;
 end;
 
+//Causes a device list refresh
 procedure TMainWnd.MIViewRefreshClick(Sender: TObject);
 begin
     //refreshing the device list
     pconnector.Refresh;
+end; //TMainWnd.MIViewRefreshClick
+
+//Exits the application
+procedure TMainWnd.TApmExitClick(Sender: TObject);
+begin
+    ExitOnClose := true;
+    self.Close;
+end; //TMainWnd.TApmExitClick
+
+procedure TMainWnd.TApmShowMainWndClick(Sender: TObject);
+begin
+
 end;
 
 initialization
